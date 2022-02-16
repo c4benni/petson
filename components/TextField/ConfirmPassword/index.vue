3e473f6cd6b281a1423b5929208b60b497d06eb1
c4benni <c4benni@gmail.com>
2022-02-16 16:57:34 +0100

    feat: create TextFieldPassword

---
+++ components/TextField/ConfirmPassword/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <TextFieldPassword v-model="vModel" :label="label" :rules="selfRules" />
+</template>
+
+<script>
+import { requiredStringProp } from '~/components/utils'
+import TextFieldMixin from '@/components/mixins/TextField'
+
+export default {
+  name: 'TextFieldConfirmPassword',
+  mixins: [TextFieldMixin],
+  props: {
+    label: {
+      type: String,
+      default: 'Confirm password *',
+    },
+    password: requiredStringProp,
+  },
+
+  computed: {
+    selfRules() {
+      console.log(3)
+      return [(v) => v === this.password || 'Passwords must match']
+    },
+  },
+}
+</script>
---
+++ components/TextField/Password/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <BaseTextField
+    v-model="vModel"
+    :type="show ? 'text' : 'password'"
+    :label="label"
+    :append-icon="appendIcon"
+    :autofocus="autofocus"
+    required
+    :rules="rules"
+    @click:append="show = !show"
+    @focus="onFocus"
+  />
+</template>
+
+<script>
+import TextFieldMixin from '@/components/mixins/TextField'
+
+export default {
+  name: 'TextFieldPassword',
+  mixins: [TextFieldMixin],
+  props: {
+    label: {
+      type: String,
+      default: 'Password *',
+    },
+  },
+
+  data: () => ({
+    show: false,
+  }),
+
+  computed: {
+    appendIcon() {
+      if (!this.vModel?.length) {
+        return undefined
+      }
+
+      return this.show ? 'mdi-eye-off' : 'mdi-eye'
+    },
+  },
+
+  methods: {
+    onFocus() {
+      if (!this.vModel) {
+        this.show = false
+      }
+    },
+  },
+}
+</script>
