4359fae310dd38319fd4463fd2affba7fdca7c88
c4benni <c4benni@gmail.com>
2022-02-16 14:25:02 +0100

    feat: update nuxt config
    Added a default meta description,
    Setup Vuetify config object

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,81 +1,104 @@
 import colors from 'vuetify/es5/util/colors'
 
+const description =
+    'Pseudo pet store. Get all your faux pet food and toys in a few clicks.'
+
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        titleTemplate: '%s - Petson',
+        title: 'test',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: description },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    titleTemplate: '%s - petson',
-    title: 'petson',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: [{ path: '~/components', extensions: ['vue'] }],
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+        // https://go.nuxtjs.dev/vuetify
+        '@nuxtjs/vuetify',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+        // https://go.nuxtjs.dev/pwa
+        '@nuxtjs/pwa',
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+        manifest: {
+            lang: 'en',
+        },
+    },
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-  ],
+    // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+    vuetify: {
+        customVariables: ['~/assets/variables.scss'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-  ],
+        theme: {
+            options: {
+                themeCache: process.env.NODE_ENV === 'production' ?
+                    {
+                        get: (key) => localStorage.getItem(key),
+                        set: (key, value) => localStorage.setItem(key, value),
+                    } :
+                    undefined,
+                customProperties: true,
+            },
+            dark: false,
+            themes: {
+                light: {
+                    anchor: '#1976D2',
+                    primary: {
+                        base: '#4EC690',
+                        lighten5: '#EDF5F1',
+                        darken4: '#00A669',
+                    },
+                    accent: colors.grey.darken3,
+                    secondary: {
+                        base: '#F09E00',
+                        lighten5: 'rgba(240, 158, 0, 0.08)',
+                        darken4: '#C66100',
+                    },
+                    info: '#2196F3',
+                    warning: colors.amber.base,
+                    error: {
+                        base: '#F44336',
+                    },
+                    success: {
+                        base: '#4CAF50',
+                    },
+                },
+            },
+        },
+    },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en'
-    }
-  },
-
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    server: {
+        port: 2022,
+        host: '0.0.0.0',
+    },
 }
