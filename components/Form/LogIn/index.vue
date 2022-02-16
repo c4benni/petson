b84108d29e1f4f3fc33a277af864371f07e896e3
c4benni <c4benni@gmail.com>
2022-02-16 17:12:56 +0100

    feat: create FormLogIn
    create Log in form component

---
+++ components/Form/LogIn/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <FormAuth checkbox-label="Remember me" action-text="Log in">
+    <BaseTextField
+      v-model="email"
+      label="Email Address *"
+      type="email"
+      required
+    />
+
+    <TextFieldPassword v-model="password" />
+
+    <template #footer>
+      <NuxtLink to="/forgot-password"> Forgot password? </NuxtLink>
+
+      <NuxtLink :to="signUpRoute"> Don't have an account? Sign up </NuxtLink>
+    </template>
+  </FormAuth>
+</template>
+
+<script>
+import { modalQuery } from '@/components/utils'
+
+export default {
+  name: 'LoginForm',
+
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+
+  computed: {
+    signUpRoute() {
+      return modalQuery.call(this, 'sign-up')
+    },
+  },
+}
+</script>
