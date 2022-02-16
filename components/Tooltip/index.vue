5104053143eb221236111bc5547b0c87dd6df074
c4benni <c4benni@gmail.com>
2022-02-16 16:49:42 +0100

    feat: create Tooltip

---
+++ components/Tooltip/index.vue
@@ -0,0 +1,58 @@
+<script>
+// Helper component to create a universal tooltip that must contain a `label` prop;
+
+export default {
+  name: 'Tooltip',
+  functional: true,
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      default: 'bottom',
+    },
+  },
+  render(h, c) {
+    const props = c.props
+
+    return h(
+      'v-tooltip',
+      {
+        props: {
+          [props.position]: true,
+        },
+        scopedSlots: {
+          activator: ({ on, attrs }) => {
+            const vNode = c.children[0]
+
+            // not a text node
+            if (typeof vNode.tag !== 'undefined' && !vNode.text) {
+              // add activator's payload to vNode;
+              // this is the equivalent of v-slot:activator="{attrs, on}" binding
+              // however, using templates removes the reactivity.
+              vNode.data = {
+                ...vNode.data,
+                attrs: {
+                  ...(vNode.data?.attrs || {}),
+                  ...attrs,
+                },
+                on: {
+                  ...(vNode.data?.on || {}),
+                  ...on,
+                },
+              }
+
+              return vNode
+            }
+
+            return null
+          },
+        },
+      },
+      [h('span', props.label)]
+    )
+  },
+}
+</script>
