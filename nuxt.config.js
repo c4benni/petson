d743457d166e45af1c95d87a3c793a4f18665b1f
c4benni <c4benni@gmail.com>
2022-02-16 17:38:16 +0100

    fix: Initial page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         titleTemplate: '%s - Petson',
-        title: 'test',
+        title: 'Faux pet store',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
