78200c7c96dc48345684db2434a1bf9313c3e5f3
c4benni <c4benni@gmail.com>
2022-02-16 16:50:08 +0100

    feat: update components/utils

--- components/utils/index.js
+++ components/utils/index.js
@@ -1,1 +1,24 @@
 export const trimmedLowerCase = (string) => string.toLowerCase().trim()
+
+export const requiredProp = (type) => ({
+    type,
+    required: true,
+})
+
+export const requiredStringProp = requiredProp(String)
+
+export const undefinedProp = (type) => ({
+    type,
+    required: true,
+})
+
+export const undefinedStringProp = undefinedProp(String)
+
+export function modalQuery(modal) {
+    return {
+        query: {
+            ...(this.$route.query || {}),
+            modal,
+        },
+    }
+}
