55237beb33b89806af6a79e3b36fa617ce7d3714
c4benni <c4benni@gmail.com>
2022-02-17 21:58:39 +0100

    feat: creat <Breadcrumbs/>
    Created breadcrumbs global component following the design specs

---
+++ components/Breadcrumbs/index.vue
@@ -0,0 +1,29 @@
+<template functional>
+  <v-breadcrumbs :items="props.items" class="p-0">
+    <template #item="{ item: { to, href, disabled, title } }">
+      <v-breadcrumbs-item
+        :to="to"
+        :href="href"
+        :target="href ? '_blank' : undefined"
+        :nuxt="!!to"
+        :disabled="disabled"
+      >
+        <UiText
+          tag="span"
+          :label="title"
+          :variant="disabled ? 'primary' : 'secondary'"
+        />
+      </v-breadcrumbs-item>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script>
+import { requiredProp } from '../utils'
+export default {
+  name: 'Breadcrumbs',
+  props: {
+    items: requiredProp(Array),
+  },
+}
+</script>
