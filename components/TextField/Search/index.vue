2b085d79c7778a88e95f46385e9fb14259dc58f9
c4benni <c4benni@gmail.com>
2022-02-17 13:19:06 +0100

    feat: create <TextFieldSearch/>

---
+++ components/TextField/Search/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <BaseTextField
+    v-model="modelSync"
+    class="Search"
+    prepend-inner-icon="mdi-magnify"
+    clearable
+    :label="placeholder"
+  />
+</template>
+
+<script>
+import TextField from '~/components/mixins/TextField'
+
+export default {
+  mixins: [TextField],
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Search',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.Search >>> fieldset,
+.Search >>> .v-icon,
+.Search >>> .v-label {
+  color: var(--v-primary-base) !important;
+}
+</style>
