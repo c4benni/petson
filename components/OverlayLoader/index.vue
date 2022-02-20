aac8d232b45e4b77d65eac81e5d7c6c7daf60fdf
c4benni <c4benni@gmail.com>
2022-02-20 01:25:51 +0100

    feat: add <Overlay/>

---
+++ components/OverlayLoader/index.vue
@@ -0,0 +1,28 @@
+<template functional>
+  <v-overlay
+    :value="props.show"
+    color="#000"
+    opacity="0.5"
+    v-bind="data.attrs"
+    class="touch-none"
+    @wheel.stop.prevent
+  >
+    <v-progress-circular
+      indeterminate
+      size="64"
+      color="secondary"
+    ></v-progress-circular>
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  name: 'OverlayLoader',
+
+  props: {
+    show: Boolean,
+  },
+}
+</script>
+
+<style></style>
