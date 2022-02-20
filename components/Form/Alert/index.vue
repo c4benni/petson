00ce6369600cefb19f05248cae60cbc1752f4919
c4benni <c4benni@gmail.com>
2022-02-20 01:27:20 +0100

    feat: create <FormAlert/>

---
+++ components/Form/Alert/index.vue
@@ -0,0 +1,47 @@
+<template functional>
+  <v-fade-transition>
+    <v-alert
+      v-if="props.error"
+      text
+      type="error"
+      color="error"
+      class="mb-[36px]"
+    >
+      <UiText
+        :label="props.error.message"
+        variant="subtitle-1"
+        opacity="custom"
+        class="error-text--text"
+      />
+
+      <template v-if="props.error.errors">
+        <ul
+          v-for="(messages, errTitle) in props.error.errors"
+          :key="errTitle"
+          :aria-label="`${errTitle} error`"
+        >
+          <UiText
+            v-for="(message, err) in messages"
+            :key="err"
+            tag="li"
+            variant="caption"
+            opacity="secondary"
+            :label="message"
+            class="list-disc"
+          />
+        </ul>
+      </template>
+    </v-alert>
+  </v-fade-transition>
+</template>
+
+<script>
+import { undefinedProp } from '~/components/utils'
+export default {
+  name: 'FormAlert',
+
+  props: {
+    error: undefinedProp(Object),
+  },
+}
+</script>
