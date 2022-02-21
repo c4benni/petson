254efbebcb12d5875ad91b9e234325d5a05ad499
c4benni <c4benni@gmail.com>
2022-02-21 21:44:03 +0100

    refactor: moved dataTable header
    moved user settings data table header to its own file

---
+++ components/UserSettings/DataTable/utils.js
@@ -0,0 +1,14 @@
+export const headers = [{
+        text: 'Order UUID',
+        value: 'uuid',
+        class: 'pl-[28px]',
+        cellClass: 'pl-[28px] text',
+    },
+    { text: 'Status', value: 'status' },
+    {
+        text: 'Download Invoice',
+        value: 'invoice',
+        align: 'right',
+        sortable: false,
+    },
+]
