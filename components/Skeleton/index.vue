8055d42cc88b8b0c3c9855c2ced651c8371e684a
c4benni <c4benni@gmail.com>
2022-02-21 21:40:29 +0100

    feat: create <Skeleton/>

---
+++ components/Skeleton/index.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <div
+    role="alert"
+    aria-busy="true"
+    aria-live="polite"
+    :class="[data.staticClass, data.class]"
+    v-bind="$attrs"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Skeleton',
+}
+</script>
