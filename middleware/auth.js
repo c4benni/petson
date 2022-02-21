9a44701a16bd5ee28db75d0e22c546b132d06198
c4benni <c4benni@gmail.com>
2022-02-21 21:34:38 +0100

    fix: middleware redirects

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,30 @@
 export default function({ store, redirect, route, req }) {
     // redirect modalQueries;
 
-    // if there's no session, and client is trying to access ?modal=user-settings, bail.
+    // if there's no session, and client is trying to access ?modal=user-settings, show login.
     const {
         user: { info },
     } = store.state
 
     const modalQuery = route.query.modal
 
-    const closeModal = () => {
+    const changeModal = (modal = '') => {
+        // change queryModal, ignore args to close
         redirect({
             query: {
                 ...route.query,
-                modal: undefined,
+                modal: modal || undefined,
             },
         })
     }
 
     if (!info && modalQuery === 'user-settings') {
-        closeModal()
+        changeModal('log-in')
     }
 
     // if logged in client is trying to access ?modal=log-in or ?modal=sign-up,
     // bail.
-
-    if (info && ['log-in', 'sign-up'].includes(modalQuery)) {
-        closeModal()
+    else if (info && ['log-in', 'sign-up'].includes(modalQuery)) {
+        changeModal()
     }
 }
