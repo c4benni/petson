8a7623f24e31410c385e5b7ef391851cca099332
c4benni <c4benni@gmail.com>
2022-02-25 15:14:41 +0100

    feat: add auto login to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,6 +20,8 @@
 
 <script>
 import { mapActions } from 'vuex'
+import smoothscroll from 'smoothscroll-polyfill'
+import { getCookie, setCookie } from '~/services/utils'
 
 export default {
   name: 'DefaultLayout',
@@ -27,6 +29,29 @@ export default {
   async fetch() {
     // build main page here in default layout, so no mater what page is visited, the main page will always be ready to display its sections.
     await this.buildMainPage()
+  },
+
+  async beforeMount() {
+    // init smoothscroll polyfill
+    smoothscroll.polyfill()
+
+    // automatic login
+    // get cookie, if it exists, set axios header, then dispatch 'user/getInfo'
+    const token = getCookie('token')
+
+    if (token) {
+      this.$axios.setHeader('Authorization', `Bearer ${token}`)
+
+      // try to update the user/info state.
+      // if it fails, clear cookies and Bearer token
+      const { error } = await this.$store.dispatch('user/getInfo')
+
+      if (error) {
+        setCookie('token', '')
+
+        this.$axios.setHeader('Authorization', '')
+      }
+    }
   },
 
   methods: {
