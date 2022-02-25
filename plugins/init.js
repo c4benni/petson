381a7212027f1766153b324a5a81730e37dd3b6a
c4benni <c4benni@gmail.com>
2022-02-25 15:10:46 +0100

    fix: app plugin
    Moved scroll-pollyfill initialization, and automatic log in to the default layout

--- plugins/init.js
+++ plugins/init.js
@@ -1,11 +1,5 @@
-import smoothscroll from 'smoothscroll-polyfill'
-import { getCookie, setCookie } from '~/services/utils'
-
 // initialize app; Add any configuration that should take place before app is mounted.
-export default async function init({ store, $axios, redirect, route }, inject) {
-    // init smoothscroll polyfill
-    smoothscroll.polyfill()
-
+export default function init({ store, $axios, redirect, route }, inject) {
     const notify = {
         async open(message, timeout) {
             await store.dispatch('app/openSnackbar', message, timeout)
@@ -16,24 +10,6 @@ export default async function init({ store, $axios, redirect, route }, inject) {
     }
 
     inject('notify', notify)
-
-    // automatic login
-    // get cookie, if it exists, set axios header, then dispatch 'user/getInfo'
-    const token = getCookie('token')
-
-    if (token) {
-        $axios.setHeader('Authorization', `Bearer ${token}`)
-
-        // try to update the user/info state.
-        // if it fails, clear cookies and Bearer token
-        const { error } = await store.dispatch('user/getInfo')
-
-        if (error) {
-            setCookie('token', '')
-
-            $axios.setHeader('Authorization', '')
-        }
-    }
 
     // catch all unauthorized requests and send client to login modal;
     $axios.onError((err) => {
