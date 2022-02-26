41f2c45999acbd771c218e35cd150aab3985537a
c4benni <c4benni@gmail.com>
2022-02-26 21:19:28 +0100

    fix: restore github pages

---
+++ docs/_nuxt/e4c42f9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{511:function(n,e,t){"use strict";t.r(e);var l={name:"MainPageContainer"},c=t(20),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",{staticClass:"px-[52px]"},[n._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
