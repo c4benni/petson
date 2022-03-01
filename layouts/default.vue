8e9980c533cd95c2f7ad994a8ff480520da47906
c4benni <c4benni@gmail.com>
2022-03-01 22:54:22 +0100

    fix: auto login
    Moved auto login logic to the default layout's created hook

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,9 +31,11 @@ export default {
     ...mapGetters('user', ['loggedIn']),
   },
 
-  async created() {
+  created() {
+    this.autoLogin()
+
     // build main page here in default layout, so no matter what page is visited, the main page will always be ready to display its sections.
-    await this.buildMainPage()
+    this.buildMainPage()
   },
 
   beforeMount() {
@@ -45,6 +47,31 @@ export default {
     ...mapActions('mainPage', {
       buildMainPage: 'build',
     }),
+
+    ...mapActions('user', ['getInfo']),
+
+    // automatic login
+    async autoLogin() {
+      // get cookie, if it exists, set axios header, then dispatch 'user/getInfo'
+      const token = this.$cookies.get('token')
+
+      if (token) {
+        this.$axios.setHeader('Authorization', `Bearer ${token}`)
+
+        // try to update the user/info state.
+        // if it fails, clear cookies and Bearer token
+        const { error } = await this.getInfo()
+
+        if (error) {
+          this.$cookies.remove('token')
+
+          // send back home
+          this.$router.replace('/')
+
+          this.$axios.setHeader('Authorization', '')
+        }
+      }
+    },
   },
 }
 </script>
